b2b934823155dd61f21cc2edbe4a85d43af09753
Kenny Stanley <kpstanleyjr@gmail.com>
2022-01-13 23:52:08 -0600

    chore: added ts-ignore

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,6 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
+// @ts-ignore
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'NextStax Docs',
